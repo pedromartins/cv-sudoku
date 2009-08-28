e0878fbe4eecd909c2562702cb68afe4f1c9c4eb
pedromartins.pt <pedromartins.pt@7fbabbaa-93c3-11de-9a30-078c9dd406eb>
2009-08-28 23:18:47 +0000

    Removed unnecessary for loop

--- trunk/CVSudoku/main.c
+++ trunk/CVSudoku/main.c
@@ -58,85 +58,79 @@ CvSeq* findSquares4( IplImage* img, CvMemStorage* storage )
     cvPyrUp( pyr, timg, 7 );
     tgray = cvCreateImage( sz, 8, 1 );
     
-    // find squares in every color plane of the image
-    for( c = 0; c < 3; c++ )
-    {
-        // extract the c-th color plane
-        //cvSetImageCOI( timg, c+1 );
-        cvCopy( timg, tgray, 0 );
-        
-        // try several threshold levels
-        for( l = 0; l < N; l++ )
-        {
-            // hack: use Canny instead of zero threshold level.
-            // Canny helps to catch squares with gradient shading   
-            if( l == 0 )
-            {
-                // apply Canny. Take the upper threshold from slider
-                // and set the lower to 0 (which forces edges merging) 
-                cvCanny( tgray, gray, 0, thresh, 5 );
-                // dilate canny output to remove potential
-                // holes between edge segments 
-                cvDilate( gray, gray, 0, 1 );
-            }
-            else
-            {
-                // apply threshold if l!=0:
-                //     tgray(x,y) = gray(x,y) < (l+1)*255/N ? 255 : 0
-                cvThreshold( tgray, gray, (l+1)*255/N, 255, CV_THRESH_BINARY );
-            }
-            
-            // find contours and store them all as a list
-            cvFindContours( gray, storage, &contours, sizeof(CvContour),
-						   CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE, cvPoint(0,0) );
-            
-            // test each contour
-            while( contours )
-            {
-                // approximate contour with accuracy proportional
-                // to the contour perimeter
-                result = cvApproxPoly( contours, sizeof(CvContour), storage,
-									  CV_POLY_APPROX_DP, cvContourPerimeter(contours)*0.02, 0 );
-                // square contours should have 4 vertices after approximation
-                // relatively large area (to filter out noisy contours)
-                // and be convex.
-                // Note: absolute value of an area is used because
-                // area may be positive or negative - in accordance with the
-                // contour orientation
-                if( result->total == 4 &&
-				   fabs(cvContourArea(result,CV_WHOLE_SEQ)) > 1000 &&
-				   cvCheckContourConvexity(result) )
-                {
-                    s = 0;
-                    
-                    for( i = 0; i < 5; i++ )
-                    {
-                        // find minimum angle between joint
-                        // edges (maximum of cosine)
-                        if( i >= 2 )
-                        {
-                            t = fabs(angle(
-										   (CvPoint*)cvGetSeqElem( result, i ),
-										   (CvPoint*)cvGetSeqElem( result, i-2 ),
-										   (CvPoint*)cvGetSeqElem( result, i-1 )));
-                            s = s > t ? s : t;
-                        }
-                    }
-                    
-                    // if cosines of all angles are small
-                    // (all angles are ~90 degree) then write quandrange
-                    // vertices to resultant sequence 
-                    if( s < 0.3 )
-                        for( i = 0; i < 4; i++ )
-                            cvSeqPush( squares,
-									  (CvPoint*)cvGetSeqElem( result, i ));
-                }
-                
-                // take the next contour
-                contours = contours->h_next;
-            }
-        }
-    }
+	cvCopy( timg, tgray, 0 );
+	
+	// try several threshold levels
+	for( l = 0; l < N; l++ )
+	{
+		// hack: use Canny instead of zero threshold level.
+		// Canny helps to catch squares with gradient shading   
+		if( l == 0 )
+		{
+			// apply Canny. Take the upper threshold from slider
+			// and set the lower to 0 (which forces edges merging) 
+			cvCanny( tgray, gray, 0, thresh, 5 );
+			// dilate canny output to remove potential
+			// holes between edge segments 
+			cvDilate( gray, gray, 0, 1 );
+		}
+		else
+		{
+			// apply threshold if l!=0:
+			//     tgray(x,y) = gray(x,y) < (l+1)*255/N ? 255 : 0
+			cvThreshold( tgray, gray, (l+1)*255/N, 255, CV_THRESH_BINARY );
+		}
+		
+		// find contours and store them all as a list
+		cvFindContours( gray, storage, &contours, sizeof(CvContour),
+					   CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE, cvPoint(0,0) );
+		
+		// test each contour
+		while( contours )
+		{
+			// approximate contour with accuracy proportional
+			// to the contour perimeter
+			result = cvApproxPoly( contours, sizeof(CvContour), storage,
+								  CV_POLY_APPROX_DP, cvContourPerimeter(contours)*0.02, 0 );
+			// square contours should have 4 vertices after approximation
+			// relatively large area (to filter out noisy contours)
+			// and be convex.
+			// Note: absolute value of an area is used because
+			// area may be positive or negative - in accordance with the
+			// contour orientation
+			if( result->total == 4 &&
+			   fabs(cvContourArea(result,CV_WHOLE_SEQ)) > 1000 &&
+			   cvCheckContourConvexity(result) )
+			{
+				s = 0;
+				
+				for( i = 0; i < 5; i++ )
+				{
+					// find minimum angle between joint
+					// edges (maximum of cosine)
+					if( i >= 2 )
+					{
+						t = fabs(angle(
+									   (CvPoint*)cvGetSeqElem( result, i ),
+									   (CvPoint*)cvGetSeqElem( result, i-2 ),
+									   (CvPoint*)cvGetSeqElem( result, i-1 )));
+						s = s > t ? s : t;
+					}
+				}
+				
+				// if cosines of all angles are small
+				// (all angles are ~90 degree) then write quandrange
+				// vertices to resultant sequence 
+				if( s < 0.3 )
+					for( i = 0; i < 4; i++ )
+						cvSeqPush( squares,
+								  (CvPoint*)cvGetSeqElem( result, i ));
+			}
+			
+			// take the next contour
+			contours = contours->h_next;
+		}
+	}
     
     // release all the temporary images
     cvReleaseImage( &gray );
@@ -357,8 +351,8 @@ int main (int argc, const char * argv[]) {
 		CvSeq *contours = findSquares4(tgray, storage);
 		
 		// Because of unknown size, needs to be allocated in the function and freed afterwards
-		//IplImage *sudoku = extractSudokuBoard(tgray, contours);
-		IplImage *sudoku = cvCloneImage(frame);
+		IplImage *sudoku = extractSudokuBoard(tgray, contours);
+		//IplImage *sudoku = cvCloneImage(frame);
 		//cvThreshold( sudoku_pre, sudoku, 10, 255, CV_THRESH_BINARY );
 		
 		CvRect square;

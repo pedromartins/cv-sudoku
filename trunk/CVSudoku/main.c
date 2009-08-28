35d060cb9d9a19b8a8788733d21d9daff9127b4a
pedromartins.pt <pedromartins.pt@7fbabbaa-93c3-11de-9a30-078c9dd406eb>
2009-08-28 13:35:47 +0000

    Squares example

--- trunk/CVSudoku/main.c
+++ trunk/CVSudoku/main.c
@@ -1,8 +1,37 @@
 
 #include "cv.h"
+#include "highgui.h"
 #include <stdio.h>
+#include <iostream>
+#include <string>
+#include <cmath>
 
-int main (int argc, const char * argv[]) {
+CvSeq *findSquares( IplImage *img, CvMemStorage *storage ) {
+	
+}
+
+int maina (int argc, const char * argv[]) {
+	CvCapture *camera = cvCreateCameraCapture(CV_CAP_ANY);
+	
+	if (! camera){
+		std::cout << "no camera" << std::endl;
+        exit(1);
+	}
+	
+	std::string winname = "";
+	for(int i = 0; i < 11; i++){
+		winname += (char) rand();
+	}
+	winname += '\0';
+	
+	cvNamedWindow(winname.c_str(), 1);	
+	
+	while(true) {
+		IplImage *frame = cvQueryFrame(camera);
+		cvShowImage(winname.c_str(), frame);
+		int key = cvWaitKey (100);
+	}
+	
     // insert code here...
     printf("Hello, changed World!\n");
     return 0;
